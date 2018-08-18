0c42f8c389c019c9800d304ea8f3e7281a1d5625
zhuanghongji <zhuanghongjichina@gmail.com>
2018-08-18 18:21:24 +0800

    体系列表完善

---
+++ src/pages/tree/TreeChildView.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  View,
+  Text,
+  FlatList,
+} from 'react-native'
+
+let screenWidth = Dimensions.get('window').width
+
+/**
+ * 组件：体系二级目录标题及其子项
+ */
+export default class TreeChildView extends Component {
+  constructor(props) {
+    super(props)
+    this.child = props.child
+  }
+
+  /**
+   * 点击 “绘制二级目录下的项” 的回调
+   * @param {*} grandchild 
+   */
+  onGrandchildPress(grandchild) {
+    this.props.onGrandchildPress(grandchild)
+  }
+
+  /**
+   * 绘制二级目录下的项
+   * @param {*} item 
+   */
+  renderGrandchild(item) {
+    // console.log('renderGrandchild', item.item.name)
+    return (
+      <TouchableOpacity 
+        style={styles.grandchildWrapper}
+        onPress={() => this.onGrandchildPress(item.item)}
+      >
+        <Text style={styles.grandchildName}>{item.item.name}</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.childWrapper}>
+            <Text style={styles.childName}>{this.child.name}</Text>
+          </View>
+
+          <FlatList
+            style={styles.flatList}
+            data={this.props.child.children}
+            keyExtractor={(grandchild) => grandchild.name}
+            numColumns={3}
+            renderItem={(item) => this.renderGrandchild(item)}
+          />
+        </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth,
+  },
+  childWrapper: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: screenWidth,
+    backgroundColor: '#efefef',
+    marginTop: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+  },
+  childName: {
+    color: 'gray',
+  },
+  flatList: {
+    width: screenWidth,
+    paddingVertical: 8,
+    backgroundColor: '#ffffff'
+  },
+  grandchildWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 28,
+    marginHorizontal: 12,
+    marginVertical: 4,
+    paddingHorizontal: 6,
+    paddingVertical: 4,
+    borderWidth: 1,
+    borderColor: '#235C87',
+    borderRadius: 14,
+  },
+  grandchildName: {
+    color: '#235C87',
+  },
+})
--- src/pages/tree/TreeScreen.js
+++ src/pages/tree/TreeScreen.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   View,
-  Text,
+  FlatList,
 } from 'react-native'
+
+import HttpManager from '../../../src/http/HttpManager'
+import TreeChildView from './TreeChildView'
 
 /**
  * 页面：体系
@@ -13,10 +16,56 @@ export default class TreeScreen extends Component {
     title: '体系',
   }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      tree: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadTreeJson()
+  }
+
+  /**
+   * 加载 “体系” 数据
+   */
+  loadTreeJson() {
+    return HttpManager.get('/tree/json')
+      .then(res => {
+        console.log('loadTreeJson success')
+        this.setState({
+          tree: res.data,
+        })
+      })
+      .catch(err => { 
+        console.log('loadTreeJson error')
+    })
+  }
+
+  /**
+   * 点击了 “体系下二级目录的子项”
+   * 
+   * @param {*} grandchild 
+   */
+  onGrandchildPress(grandchild) {
+    alert(grandchild.name)
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>体系</Text>
+        <FlatList
+          style={styles.flatList}
+          data={this.state.tree}
+          keyExtractor={(treeChind) => treeChind.name}
+          renderItem={({ item }) => (
+            <TreeChildView 
+              child={item} 
+              onGrandchildPress={(grandchild) => {this.onGrandchildPress(grandchild)}}
+            />
+          )}
+        />
       </View>
     )
   }
@@ -25,8 +74,11 @@ export default class TreeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#EFEFEF',
   },
+  flatList: {
+    flex: 1
+  }
 });
