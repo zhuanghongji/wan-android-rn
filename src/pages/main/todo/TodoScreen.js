2ee1ef080a5356ffb87084a2efdc7a22ad931600
zhuanghongji <zhuanghongjichina@gmail.com>
2019-02-14 19:32:21 +0800

    add TypeScript

--- src/pages/main/todo/TodoScreen.js
+++ src/pages/main/todo/TodoScreen.js
@@ -5,6 +5,8 @@ import {
   Text,
 } from 'react-native'
 
+// import { Hello } from '../../../components/Hello'
+
 /**
  * 首页 - 待办页面
  */
@@ -17,6 +19,7 @@ export default class ArticleScreen extends Component {
     return (
       <View style={styles.container}>
         <Text>待办</Text>
+        {/* <Hello name="zhuang" enthusiasmLevel={1} /> */}
       </View>
     )
   }
